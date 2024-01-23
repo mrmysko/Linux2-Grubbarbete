0d97712c70a29de4e8e73fa00c8b0098edd51894
MrMysko <orkar_-.--@hotmail.com>
2024-01-23 11:58:23 +0100

    lderp

--- ldap_scripts/import_from_csv.sh
+++ ldap_scripts/import_from_csv.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Todo - Split domain and edit dn.
-# Todo - Om den här csvn ska importeras i en existerande db, hur vet jag vilka uidNr som inte är upptagna? Och vilka gid som är rätt grupper.
+# Todo - Om den här csvn ska importeras i en existerande db, hur vet jag vilka uidNr som inte är upptagna? Och vilka gid som är rätt?
+# Todo - Kolla om ett värde är tomt, ta bort raden om det är de. LDAP kan inte importera om rader är tomma.
+#        Kanske läs första raden i csvn och sätt variabler efter den?
 
 # USAGE: import_from_csv.sh <domain> <input.csv> <output.ldif>
 
@@ -19,12 +21,12 @@ echo cn: "$cn"
 echo loginShell: /bin/false
 echo homeDirectory: /home/"$uid"
 echo uidNumber: "$uidNumber"
-echo description: "$description"
+echo description: Temp Description
 echo sn: "$sn"
 echo givenName: "$givenName"
 echo mail: "$mail"
 echo telephoneNumber: "$telephoneNumber"
-echo userPassword:: "$userPassword"
+echo userPassword:: e1NTSEF9ZS9RR2JHcGNrb0RhOEx0R1lOZFgvY1dObG9CaFNFZGs=
 echo gidNumber: "$gidNumber"
 echo
 
