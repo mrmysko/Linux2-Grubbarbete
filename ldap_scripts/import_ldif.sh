be80f2138f4f1ec2963fd950b2a920da38c9b5ae
MrMysko <orkar_-.--@hotmail.com>
2024-01-23 15:54:28 +0100

    lderp

--- ldap_scripts/import_ldif.sh
+++ ldap_scripts/import_ldif.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# Requires ldap-utils package.
+# Todo - Om jag importerar en användare sså har den en gidNumber, måste kolla om användarna är med i gruppen (gidNumber) \
+#        om inte: Lägg till användarens uid till objectClass=posixGroup memberUid
+#        Sök efter (&(objectClass=posixGroup)(gidNumber=$USER_GID)) testa om uid finns i memberUid. ldapmodify om den inte finns.
 
 docker cp "$1" ldap:.
 docker exec ldap ldapadd -c -x -y /run/secrets/ldap_admin_password -D "cn=admin,dc=hemlis,dc=com" -f "$1"
